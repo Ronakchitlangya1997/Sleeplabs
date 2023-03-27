6ec2a046b363f28124d05f2e3c2d14ec123556fd
Manvendra Singh <16uec063@lnmiit.ac.in>
2023-03-27 20:57:14 +0530

    Updated Bulk API Code to store the data in a compressed format

--- SleepLabs/models.py
+++ SleepLabs/models.py
@@ -2,6 +2,8 @@ from django.db import models
 import uuid
 from Authentication.models import User
 
+from django.db.models import JSONField
+
 # Create your models here.
 class SleepLab(models.Model):
     id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
@@ -12,8 +14,20 @@ class SleepLab(models.Model):
     GyY = models.CharField(max_length=100, default='None') 
     GyZ = models.CharField(max_length=100, default='None')
     OCC = models.CharField(max_length=100, default=1)
-    timestamp = models.DateTimeField(auto_now_add=True)
-    #Add the following at line 7
+    timestamp = models.CharField(max_length=122, default='01 June, 2022')
+    hrs = models.CharField(max_length=122, default='01 June, 2022')
+    min = models.CharField(max_length=122, default='01 June, 2022')
+    jsonData = JSONField(default=list)
+    
     DevID = models.CharField(max_length=20, default='None')
     class Meta:
-        ordering = ('timestamp', )
+        ordering = ('-timestamp', )
+
+class SleepLabOptv1(models.Model):
+    auto_increment_id = models.AutoField(primary_key=True)
+    timestamp = models.CharField(max_length=122, default='01 June, 2022')
+    jsonData = JSONField(default=list)
+    DevID = models.CharField(max_length=20, default='None')
+    class Meta:
+        ordering = ('-timestamp', )
+
